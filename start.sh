389d237a10287cc7011b5a7078997d57841e1195
bilo123 <82114684+bilo123@users.noreply.github.com>
2025-05-23 17:27:18 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # تعيين عنوان الاستضافة والبورت المطلوب
-export OLLAMA_HOST=0.0.0.0:11434
+export OLLAMA_HOST=0.0.0.0:$PORT
 
 echo "Starting Ollama service..."
 ollama serve &
 sleep 5
 
 echo "Adding models..."
-ollama pull tinyllama
+# ollama pull tinyllama
 
 echo "Listing available models..."
 ollama list
